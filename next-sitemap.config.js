cf3f1c06a49d2d0c77c5113e612d65d38a1439c8
SofianBelbacha <sofianbelbacha@gmail.com>
2025-05-07 11:03:06 +0200

    Buil du projet pour la gestion du sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://sofianbelbacha.vercel.app", // remplace par ton URL
+  siteUrl: "https://sofianbelbacha.vercel.app",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
